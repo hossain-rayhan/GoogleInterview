604201edcdb4d80e56ca95eca9f23a6935203c82
hossain-rayhan <hossain.rayhan@outlook.com>
2019-01-15 18:08:23 -0500

    Fibonacci implemented in two ways: Recursive and Memoization

---
+++ Fibonacci.java
@@ -0,0 +1,30 @@
+//Find the nth Fibonacci number
+//Developed By: Rayhan Hossain (Mukla.C) on 2019/01/15
+
+import java.util.Scanner;
+
+class Fibonacci{
+	public static int[] memo = new int[100];
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n = sc.nextInt();
+		
+		System.out.println("Nth Fibonacci: " + fibo(n));
+		System.out.println("Nth Fibonacci Memo: " + fiboMemoization(n));
+
+	}
+
+	public static int fibo(int n){
+		return n <= 1? n : fibo(n-1) + fibo(n-2);
+	}
+
+	public static int fiboMemoization(int n){
+		if(n <= 1)
+			return n;
+		if(memo[n] < 1){
+			memo[n] = fiboMemoization(n-1) + fiboMemoization(n-2);
+		}
+		return memo[n];
+	}
+}
