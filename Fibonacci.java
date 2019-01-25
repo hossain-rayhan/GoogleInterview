84b30b8dc55bd9603ff0b94da2bf17d92430f94e
hossain-rayhan <hossain.rayhan@outlook.com>
2019-01-25 02:16:59 -0500

    Fibonacci: DP with and without memoization; and Without DP

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,7 +1,7 @@
 //Find the nth Fibonacci number
 //Developed By: Rayhan Hossain (Mukla.C) on 2019/01/15
-//Without memoization Time Complexity: O(2^N)
-//With Memoization Time Complexity: O(N)- Linear
+//Without memoization Time Complexity: slight less than O(2^N)
+//With Memoization Time Complexity: almost O(N)- Linear
 
 import java.util.Scanner;
 
@@ -14,7 +14,7 @@ class Fibonacci{
 		
 		System.out.println("Nth Fibonacci: " + fibo(n));
 		System.out.println("Nth Fibonacci Memo: " + fiboMemoization(n));
-
+		System.out.println("Nth Fibonacci Without DP: " + fiboWithoutDp(n));
 	}
 
 	public static int fibo(int n){
@@ -28,5 +28,24 @@ class Fibonacci{
 			memo[n] = fiboMemoization(n-1) + fiboMemoization(n-2);
 		}
 		return memo[n];
+	}
+
+	//We can also solve this without Dynamic Programming
+	//We just can use some extra variables
+	//Though it looks simple in this case, but dynamic programming makes life easier in many cases
+	//Time Complexity would be: O(N) -Linear
+	
+	public static int fiboWithoutDp(int n){
+		if(n == 0 || n == 1)
+			return n;
+
+		int a = 0, b = 1, c = 0;
+
+		for(int i = 2; i <= n; i++){
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		return c;
 	}
 }
